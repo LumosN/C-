f854808d53206b7b374eac2e0efb62701b4e4e89
LumosN <621034090@qq.com>
2021-03-31 21:15:57 +0800

    118.杨辉三角
    给定一个非负整数 numRows，生成杨辉三角的前numRows行

---
+++ test_3_31.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        //vector<vector<int>> vv(numRows, vector<int>());
+
+        vector<vector<int>> vv;
+        vv.resize(numRows, vector<int>());//ÄäÃû¶ÔÏó
+        for(size_t i = 0; i < vv.size(); i++)
+        {
+            vv[i].resize(i + 1, 0);
+            vv[i][0] = 1;
+            vv[i][vv[i].size() - 1] = 1;
+        }
+        
+        for(size_t i = 0; i < vv.size(); i++)
+        {
+            for(size_t j = 0; j < vv[i].size(); j++)
+            {
+                if(vv[i][j] == 0)
+                {
+                    vv[i][j] = vv[i-1][j-1] + vv[i-1][j];
+                }
+            }
+        }
+        return vv;
+    }
+};
